294167c3811f2d3f6574bd87d60be14abd38ecae
muqianjue <yyj2809801816@163.com>
2024-07-22 15:38:22 +0800

    Add files via upload

--- 2.py
+++ 2.py
@@ -197,6 +197,72 @@ def df_to_xmind(df, save_dir):
 
     return full_path
 
+def export(base64 , path):
+    content = f"""
+                    <style>
+                        .download-button {{
+                            display: inline-flex;
+                            -webkit-box-align: center;
+                            align-items: center;
+                            -webkit-box-pack: center;
+                            justify-content: center;
+                            font-weight: 400;
+                            font-size: 1rem
+                            padding: 0px;
+                            border-radius: 0.5rem;
+                            min-height: 2.5rem;
+                            margin: -5px;
+                            line-height: 1.6;
+                            color: inherit;
+                            width: auto;
+                            user-select: none;
+                            background-color: rgb(255, 255, 255);
+                            border: 1px solid rgba(49, 51, 63, 0.2);
+
+                    }}
+                    .download-button:hover {{
+                        background-color: #45a049;
+                    }}
+                    </style>
+
+                        <div data-testid="column" class="st-emotion-cache-keje6w e1f1d6gn3" style="display: flex; justify-content: center; align-items: center; height: 100%;">
+                          <div data-testid="stVerticalBlockBorderWrapper" data-test-scroll-behavior="normal" class="st-emotion-cache-0 e1f1d6gn0">
+                            <div class="st-emotion-cache-1wmy9hl e1f1d6gn1">
+                              <div width="344" data-testid="stVerticalBlock" class="st-emotion-cache-1njjmvq e1f1d6gn2">
+                                <div data-stale="false" width="344" class="element-container st-emotion-cache-lj8h43 e1f1d6gn4" data-testid="element-container">
+                                  <div class="row-widget stDownloadButton" data-testid="stDownloadButton" style="width: 344px;">
+                                    <button id="downloadButton" kind="secondary" data-testid="baseButton-secondary" class="download-button" style="width: 100%;">
+                                      <div data-testid="stMarkdownContainer" class="st-emotion-cache-187vdiz e1nzilvr4">
+                                        <p>
+                                          <font style="vertical-align: inherit;">
+                                            <font style="vertical-align: inherit;">下载 Freemind 格式文件</font>
+                                          </font>
+                                        </p>
+                                      </div>
+                                    </button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                        <script>
+                        var fileBase64 = "{base64}";
+                        var fileName = "{os.path.basename(path)}";
+
+                        document.getElementById("downloadButton").addEventListener("click", function() {{
+                            var link = document.createElement("a");
+                            link.href = "data:application/octet-stream;base64," + fileBase64;
+                            link.download = fileName;
+                            document.body.appendChild(link);
+                            link.click();
+                            document.body.removeChild(link);
+                        }});
+                        </script>
+                        """
+    return content
+
 
 async def rag(requirement_info):
     # API URL
@@ -440,222 +506,24 @@ if not st.session_state.result_df.empty:
 
 
     with tempfile.TemporaryDirectory() as save_dir:
-        # 生成XMind文件
+        # 生成path文件
         xmind_path = df_to_xmind(st.session_state.result_df, save_dir)
-
+        freemind_path = df_to_freemind(st.session_state.result_df, save_dir)
+        markdown_path = df_to_markdown(st.session_state.result_df, save_dir)
         # 获取文件的base64编码
         xmind_file_base64 = get_file_base64(xmind_path)
-
-        # 创建自定义HTML和JavaScript
-        xmind_html_content = f"""
-        <style>
-            .download-button {{
-                display: inline-flex;
-                -webkit-box-align: center;
-                align-items: center;
-                -webkit-box-pack: center;
-                justify-content: center;
-                font-weight: 400;
-                font-size: 1rem
-                padding: 0px;
-                border-radius: 0.5rem;
-                min-height: 2.5rem;
-                margin: 0px;
-                line-height: 1.6;
-                color: inherit;
-                width: auto;
-                user-select: none;
-                background-color: rgb(255, 255, 255);
-                border: 1px solid rgba(49, 51, 63, 0.2);
-                
-        }}
-        .download-button:hover {{
-            background-color: #45a049;
-        }}
-        </style>
-
-            <div data-testid="column" class="st-emotion-cache-keje6w e1f1d6gn3" style="display: flex; justify-content: center; align-items: center; height: 100%;">
-              <div data-testid="stVerticalBlockBorderWrapper" data-test-scroll-behavior="normal" class="st-emotion-cache-0 e1f1d6gn0">
-                <div class="st-emotion-cache-1wmy9hl e1f1d6gn1">
-                  <div width="344" data-testid="stVerticalBlock" class="st-emotion-cache-1njjmvq e1f1d6gn2">
-                    <div data-stale="false" width="344" class="element-container st-emotion-cache-lj8h43 e1f1d6gn4" data-testid="element-container">
-                      <div class="row-widget stDownloadButton" data-testid="stDownloadButton" style="width: 344px;">
-                        <button id="downloadButton" kind="secondary" data-testid="baseButton-secondary" class="download-button" style="width: 100%;">
-                          <div data-testid="stMarkdownContainer" class="st-emotion-cache-187vdiz e1nzilvr4">
-                            <p>
-                              <font style="vertical-align: inherit;">
-                                <font style="vertical-align: inherit;">下载 XMind 文件</font>
-                              </font>
-                            </p>
-                          </div>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <script>
-            var fileBase64 = "{xmind_file_base64}";
-            var fileName = "{os.path.basename(xmind_path)}";
-
-            document.getElementById("downloadButton").addEventListener("click", function() {{
-                var link = document.createElement("a");
-                link.href = "data:application/octet-stream;base64," + fileBase64;
-                link.download = fileName;
-                document.body.appendChild(link);
-                link.click();
-                document.body.removeChild(link);
-            }});
-            </script>
-            """
-
-        # 使用 components.html 来渲染 HTML 和执行 JavaScript
-        components.html(xmind_html_content)
-
-        # 生成freemind文件
-        freemind_path = df_to_freemind(st.session_state.result_df, save_dir)
-
         freemind_file_base64 = get_file_base64(freemind_path)
-
-        freemind_html_content = f"""
-                <style>
-                    .download-button {{
-                        display: inline-flex;
-                        -webkit-box-align: center;
-                        align-items: center;
-                        -webkit-box-pack: center;
-                        justify-content: center;
-                        font-weight: 400;
-                        font-size: 1rem
-                        padding: 0px;
-                        border-radius: 0.5rem;
-                        min-height: 2.5rem;
-                        margin: 0px;
-                        line-height: 1.6;
-                        color: inherit;
-                        width: auto;
-                        user-select: none;
-                        background-color: rgb(255, 255, 255);
-                        border: 1px solid rgba(49, 51, 63, 0.2);
-
-                }}
-                .download-button:hover {{
-                    background-color: #45a049;
-                }}
-                </style>
-
-                    <div data-testid="column" class="st-emotion-cache-keje6w e1f1d6gn3" style="display: flex; justify-content: center; align-items: center; height: 100%;">
-                      <div data-testid="stVerticalBlockBorderWrapper" data-test-scroll-behavior="normal" class="st-emotion-cache-0 e1f1d6gn0">
-                        <div class="st-emotion-cache-1wmy9hl e1f1d6gn1">
-                          <div width="344" data-testid="stVerticalBlock" class="st-emotion-cache-1njjmvq e1f1d6gn2">
-                            <div data-stale="false" width="344" class="element-container st-emotion-cache-lj8h43 e1f1d6gn4" data-testid="element-container">
-                              <div class="row-widget stDownloadButton" data-testid="stDownloadButton" style="width: 344px;">
-                                <button id="downloadButton" kind="secondary" data-testid="baseButton-secondary" class="download-button" style="width: 100%;">
-                                  <div data-testid="stMarkdownContainer" class="st-emotion-cache-187vdiz e1nzilvr4">
-                                    <p>
-                                      <font style="vertical-align: inherit;">
-                                        <font style="vertical-align: inherit;">下载 Freemind 格式文件</font>
-                                      </font>
-                                    </p>
-                                  </div>
-                                </button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                    <script>
-                    var fileBase64 = "{freemind_file_base64}";
-                    var fileName = "{os.path.basename(freemind_path)}";
-
-                    document.getElementById("downloadButton").addEventListener("click", function() {{
-                        var link = document.createElement("a");
-                        link.href = "data:application/octet-stream;base64," + fileBase64;
-                        link.download = fileName;
-                        document.body.appendChild(link);
-                        link.click();
-                        document.body.removeChild(link);
-                    }});
-                    </script>
-                    """
-
-        # 使用 components.html 来渲染 HTML 和执行 JavaScript
-        components.html(freemind_html_content)
-
-        # 生成Markdown文件
-        markdown_path = df_to_markdown(st.session_state.result_df, save_dir)
-
         markdown_file_base64 = get_file_base64(markdown_path)
-
-        markdown_html_content = f"""
-                        <style>
-                            .download-button {{
-                                display: inline-flex;
-                                -webkit-box-align: center;
-                                align-items: center;
-                                -webkit-box-pack: center;
-                                justify-content: center;
-                                font-weight: 400;
-                                font-size: 1rem
-                                padding: 0px;
-                                border-radius: 0.5rem;
-                                min-height: 2.5rem;
-                                margin: 0px;
-                                line-height: 1.6;
-                                color: inherit;
-                                width: auto;
-                                user-select: none;
-                                background-color: rgb(255, 255, 255);
-                                border: 1px solid rgba(49, 51, 63, 0.2);
-
-                        }}
-                        .download-button:hover {{
-                            background-color: #45a049;
-                        }}
-                        </style>
-
-                            <div data-testid="column" class="st-emotion-cache-keje6w e1f1d6gn3" style="display: flex; justify-content: center; align-items: center; height: 100%;">
-                              <div data-testid="stVerticalBlockBorderWrapper" data-test-scroll-behavior="normal" class="st-emotion-cache-0 e1f1d6gn0">
-                                <div class="st-emotion-cache-1wmy9hl e1f1d6gn1">
-                                  <div width="344" data-testid="stVerticalBlock" class="st-emotion-cache-1njjmvq e1f1d6gn2">
-                                    <div data-stale="false" width="344" class="element-container st-emotion-cache-lj8h43 e1f1d6gn4" data-testid="element-container">
-                                      <div class="row-widget stDownloadButton" data-testid="stDownloadButton" style="width: 344px;">
-                                        <button id="downloadButton" kind="secondary" data-testid="baseButton-secondary" class="download-button" style="width: 100%;">
-                                          <div data-testid="stMarkdownContainer" class="st-emotion-cache-187vdiz e1nzilvr4">
-                                            <p>
-                                              <font style="vertical-align: inherit;">
-                                                <font style="vertical-align: inherit;">下载 Markdown 格式文件</font>
-                                              </font>
-                                            </p>
-                                          </div>
-                                        </button>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-
-                            <script>
-                            var fileBase64 = "{markdown_file_base64}";
-                            var fileName = "{os.path.basename(markdown_path)}";
-
-                            document.getElementById("downloadButton").addEventListener("click", function() {{
-                                var link = document.createElement("a");
-                                link.href = "data:application/octet-stream;base64," + fileBase64;
-                                link.download = fileName;
-                                document.body.appendChild(link);
-                                link.click();
-                                document.body.removeChild(link);
-                            }});
-                            </script>
-                            """
-
+        # 创建自定义HTML和JavaScript
+        xmind_html_content = export(xmind_file_base64 , xmind_path)
+        freemind_html_content = export(freemind_file_base64 , freemind_path)
+        markdown_html_content = export(markdown_file_base64 , markdown_path)
         # 使用 components.html 来渲染 HTML 和执行 JavaScript
+        # 生成xmind文件
+        components.html(xmind_html_content)
+        # 生成freemind文件
+        components.html(freemind_html_content)
+        # 生成Markdown文件
         components.html(markdown_html_content)
 
 st.markdown("</div>", unsafe_allow_html=True)
