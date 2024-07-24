85676f830b4cb92f03ab1b863b0e61679631e463
chenjun_e <1064789374@qq.com>
2024-07-24 18:12:53 +0800

    修改记录表字段

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -20,5 +20,6 @@ CREATE TABLE IF NOT EXISTS actg_record_batch (
     file_hash_code VARCHAR(256) COMMENT '需求文件哈希码',
     upload_time TIMESTAMP COMMENT '开始上传时间',
     last_analyze_time TIMESTAMP COMMENT '最近一次分析时间',
-    status VARCHAR(64) COMMENT '状态（0是已上传，1是已分析，2是已删除）'
+    status VARCHAR(4) COMMENT '状态（0是已上传，1是已保存，2是已分析）',
+    deleted VARCHAR(4) COMMENT '状态（0是未删除，1是已删除）'
 ) COMMENT '历史记录表';
